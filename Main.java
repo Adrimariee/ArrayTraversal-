cdd8a590179660980aea55e433c9c93ecd1c71e3
Adrienne Davi <adrienne.davi@patelhs.org>
2020-11-17 13:18:06 +0000

    Finished string

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 class Main {
   public static void main(String[] args) {
-    String[] Students = {"Adam", "Dexter", "Marcus", "Gabby", "Elianna", "Katie" ,"Merida" "Anna","Brayn"};
+    String[] Students = {"Adam", "Dexter", "Marcus", "Gabby", "Elianna", "Katie" ,"Merida" "Anna","Brayn","Sunny","Barnaby","Madison","Eliot","Zayne","Maya"};
     
     for(int i=0;i<7;i++){
       System.out.println( "The candy is  " + candy [i]);

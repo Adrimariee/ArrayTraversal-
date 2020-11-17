2193cdb43c4c961757cb90c4945f2e7ab041ebc6
Adrienne Davi <adrienne.davi@patelhs.org>
2020-11-17 13:14:18 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,16 @@
+class Main {
+  public static void main(String[] args) {
+    String[] candy = {"Adam", "Dexter", "Marcus", "Gabby", "Elianna", "Katie" ,"Merida" "Anna"};
+    
+    for(int i=0;i<7;i++){
+      System.out.println( "The candy is  " + candy [i]);
+    }
+
+    candy[6]= "crunch";
+
+    for(int i=0; i<7; i++){ 
+      System.out.println("The update is " + candy[i]);
+    }
+
+  }
+}

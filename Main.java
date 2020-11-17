7acce889bd4e54ecbe4ee8cb8b9b358360e7c12d
Adrienne Davi <adrienne.davi@patelhs.org>
2020-11-17 13:16:12 +0000

    changed to "students"

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 class Main {
   public static void main(String[] args) {
-    String[] candy = {"Adam", "Dexter", "Marcus", "Gabby", "Elianna", "Katie" ,"Merida" "Anna","Brayn"};
+    String[] Students = {"Adam", "Dexter", "Marcus", "Gabby", "Elianna", "Katie" ,"Merida" "Anna","Brayn"};
     
     for(int i=0;i<7;i++){
       System.out.println( "The candy is  " + candy [i]);
